b09f4cc4dc6ef837fb6db7406e53a10b31c12085
Flaresjr <93064116+Flaresjr@users.noreply.github.com>
2021-11-10 12:12:15 -0300

    Create questão02.sh

---
+++ questão02.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+read -p "Escreva um nome de um diretório: " dir1
+read -p "Escreva um nome de um diretório: " dir2
+read -p "Escreva um nome de um diretório: " dir3
+read -p "Escreva um nome de um diretório: " dir4
+read -p "Escreva um nome de um diretório: " dir5
+
+
+echo $dir1
+ls $dir1
+
+echo $dir2
+ls $dir2
+
+echo $dir3
+ls $dir3
+
+echo $dir4
+ls $dir4
+
+echo $dir5
+ls $dir5
