7998baadad88a62b9a41231d152308e05ef26c35
Flaresjr <93064116+Flaresjr@users.noreply.github.com>
2021-11-10 22:09:41 -0300

    Update questão02.sh

--- questão02.sh
+++ questão02.sh
@@ -3,7 +3,7 @@ read -p "Escreva um nome de um diretório: " dir1
 read -p "Escreva um nome de um diretório: " dir2
 read -p "Escreva um nome de um diretório: " dir3
 read -p "Escreva um nome de um diretório: " dir4
-read -p "Escreva um nome de um diretório: " dir5
+
 
 
 echo $dir1
@@ -18,5 +18,4 @@ ls $dir3
 echo $dir4
 ls $dir4
 
-echo $dir5
-ls $dir5
+
